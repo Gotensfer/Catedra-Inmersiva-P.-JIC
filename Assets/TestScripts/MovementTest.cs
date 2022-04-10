6fd38d4aa058474f2e5b06dfd28ec673f020a1c5
Jose Daniel Montoya Molina <66889936+panchuel@users.noreply.github.com>
2022-04-10 18:04:19 -0500

    CORRECCIÓN: Movimiento de los barcos

---
+++ Assets/TestScripts/MovementTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTest : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() 
+    {
+        rb.AddForce(transform.forward * speed, ForceMode.Force);
+    }
+}
